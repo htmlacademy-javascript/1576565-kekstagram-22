5a2e63813cba04a3f4ea21fd74a3868385320aad
Keks <keks@htmlacademy.ru>
2021-03-03 22:57:48 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,12 @@
+import './utils.js';
+import {generateData} from './data.js';
+import './form.js';
+import './big-picture.js';
+import './picture.js';
+import './slider.js';
+
+
+// eslint-disable-next-line no-unused-vars
+const pics = generateData();
+
+
---
+++ 7/js/picture.js
@@ -0,0 +1,55 @@
+import {generateData} from './data.js';
+import {openModal} from './big-picture.js';
+
+const pictureListElem = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const createMiniPicture = ({id, url, comments, likes}) => {
+  const pictureElem = pictureTemplate.cloneNode(true);
+
+  pictureElem.dataset.id = id;
+  pictureElem.querySelector('.picture__img').src = url;
+  pictureElem.querySelector('.picture__comments').textContent = comments.length;
+  pictureElem.querySelector('.picture__likes').textContent = likes;
+
+  return pictureElem;
+}
+
+const createMiniPictures = (pictures) => {
+  const pictureFragment = document.createDocumentFragment();
+
+  pictures.forEach((item) => {
+    pictureFragment.appendChild(createMiniPicture(item));
+
+  });
+
+  return pictureFragment
+};
+
+const pics = generateData();
+const miniPictures = createMiniPictures(pics);
+
+pictureListElem.appendChild(miniPictures);
+
+pictureListElem.addEventListener('click', (evt) => {
+  if(evt.target.closest('a.picture')){
+    evt.preventDefault()
+    const pic = evt.target.closest('a.picture')
+    const picId = pic.dataset.id
+    const picture = pics.find(item => item.id === picId)
+    openModal(picture);
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
