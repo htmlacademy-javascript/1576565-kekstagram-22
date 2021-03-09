b3bdb036d7e57b1c746a1f20dbf690c0d8f25fcb
Keks <keks@htmlacademy.ru>
2021-03-10 00:13:48 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,13 @@
+import './utils.js';
+import {generateData} from './data.js';
+import './form.js';
+import './big-picture.js';
+import './picture.js';
+import './slider.js';
+import './validation.js';
+
+
+// eslint-disable-next-line no-unused-vars
+const pics = generateData();
+
+
